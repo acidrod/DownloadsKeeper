af45d6cf2c1da5475d8242b0bbcc7d827c9183ba
acidrod <alarcon.rodrigo@gmail.com>
2018-12-04 11:52:50 -0300

    Add project files.

---
+++ MainService/Keeper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Timers;
+
+// Informacion de ConfigurationManager en https://goo.gl/M6THsb
+
+namespace MainService
+{
+    public class Keeper
+    {
+        private readonly Timer _timer;
+        private readonly string downloadsPath = KnownFolders.GetPath(KnownFolder.Downloads);
+        private int daysOld;
+        private readonly string tempPath = Path.GetTempPath();
+
+        private string ReadRunPeriod()
+        {
+            var appSetting = ConfigurationManager.AppSettings;
+            daysOld = int.Parse(appSetting["MaxFileDays"]);
+            return appSetting["RunPeriod"];
+        }
+
+        public Keeper()
+        {
+            _timer = new Timer(double.Parse(ReadRunPeriod()));
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(downloadsPath);
+            var Files = dir.GetFiles("*.*").Where(f => DateTime.Now.Subtract(f.CreationTime).Days >= daysOld).OrderBy(f => f.CreationTime);
+            
+            if (Files.Count() >= 1)
+            {
+                foreach (FileInfo file in Files)
+                {
+                    LogDelete(file.Name);
+                    file.Delete();
+                }
+            }
+        }
+
+        public void LogDelete(string DeletedFileName){ 
+            string[] lines = null;
+            lines = new string[] {$"Deleted {DeletedFileName} on " + DateTime.Now.ToString()};
+            File.AppendAllLines($@"{tempPath}\DownloadsKeeper\FilesDeleted.log", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ MainService/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace MainService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usando como guia desde https://goo.gl/9hqr97
+
+            Console.WriteLine();
+        }
+    }
+}
